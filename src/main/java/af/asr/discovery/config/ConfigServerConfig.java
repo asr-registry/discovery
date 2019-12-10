40adf9c25c1826636ca733899a323c88cbf6c219
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-10 09:42:23 +0430

    hazelcast in-memory data grid

---
+++ src/main/java/af/asr/discovery/config/ConfigServerConfig.java
@@ -0,0 +1,9 @@
+package af.asr.discovery.config;
+
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigServer
+public class ConfigServerConfig {
+}
