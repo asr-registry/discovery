fe111dc4243269072b9ff6847b43056f7966a04d
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-10 09:30:23 +0430

    setup

---
+++ src/main/java/af/asr/phoenixdiscovery/PhoenixDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package af.asr.phoenixdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoenixDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoenixDiscoveryApplication.class, args);
+	}
+
+}
