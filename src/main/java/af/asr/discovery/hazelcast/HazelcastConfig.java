6fb96c00a3bcebc520491817c134ba81ababd85c
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-10 09:49:54 +0430

    hazelcast in-memory data grid

--- src/main/java/af/asr/discovery/hazelcast/HazelcastConfig.java
+++ src/main/java/af/asr/discovery/hazelcast/HazelcastConfig.java
@@ -9,16 +9,73 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HazelcastConfig{
+//    @Bean
+//    public Config hazelCastConfig(){
+//        Config config = new Config();
+//        config.setInstanceName("hazelcast-instance")
+//                .addMapConfig(
+//                        new MapConfig()
+//                                .setName("configuration")
+//                                .setMaxSizeConfig(new MaxSizeConfig(200, MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE))
+//                                .setEvictionPolicy(EvictionPolicy.LRU)
+//                                .setTimeToLiveSeconds(-1));
+//        return config;
+//    }
+
     @Bean
-    public Config hazelCastConfig(){
+    public Config hazelcastConfig() {
         Config config = new Config();
-        config.setInstanceName("hazelcast-instance")
-                .addMapConfig(
-                        new MapConfig()
-                                .setName("configuration")
-                                .setMaxSizeConfig(new MaxSizeConfig(200, MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE))
-                                .setEvictionPolicy(EvictionPolicy.LRU)
-                                .setTimeToLiveSeconds(-1));
+        config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
+        config.getNetworkConfig().getJoin().getEurekaConfig()
+                .setEnabled(true)
+                .setProperty("self-registration", "true")
+                .setProperty("namespace", "hazelcast");
         return config;
     }
+
+
+//    @Bean
+//    public Config hazelcastConfig(EurekaClient eurekaClient) {
+//        EurekaOneDiscoveryStrategyFactory.setEurekaClient(eurekaClient);
+//        Config config = new Config();
+//        config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
+//        config.getNetworkConfig().getJoin().getEurekaConfig()
+//                .setEnabled(true)
+//                .setProperty("self-registration", "true")
+//                .setProperty("namespace", "hazelcast")
+//                .setProperty("use-metadata-for-host-and-port", "true");
+//        return config;
+//    }
+
+
+//    @Bean
+//    public Config hazelcast() {
+//        MapConfig eventStoreMap = new MapConfig("spring-boot-admin-event-store")
+//                .setInMemoryFormat(InMemoryFormat.OBJECT)
+//                .setBackupCount(1)
+//                .setEvictionPolicy(EvictionPolicy.NONE)
+//                .setMergePolicyConfig(new MergePolicyConfig(PutIfAbsentMapMergePolicy.class.getName(), 100));
+//
+//        MapConfig sentNotificationsMap = new MapConfig("spring-boot-admin-application-store")
+//                .setInMemoryFormat(InMemoryFormat.OBJECT)
+//                .setBackupCount(1)
+//                .setEvictionPolicy(EvictionPolicy.LRU)
+//                .setMergePolicyConfig(new MergePolicyConfig(PutIfAbsentMapMergePolicy.class.getName(), 100));
+//
+//        Config config = new Config();
+//        config.addMapConfig(eventStoreMap);
+//        config.addMapConfig(sentNotificationsMap);
+//        config.setProperty("hazelcast.jmx", "true");
+//
+//        config.getNetworkConfig()
+//                .getJoin()
+//                .getMulticastConfig()
+//                .setEnabled(false);
+//        TcpIpConfig tcpIpConfig = config.getNetworkConfig()
+//                .getJoin()
+//                .getTcpIpConfig();
+//        tcpIpConfig.setEnabled(true);
+//        tcpIpConfig.setMembers(Collections.singletonList("127.0.0.1"));
+//        return config;
+//    }
 }
