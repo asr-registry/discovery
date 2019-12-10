17c8fe59e046cbf1eae44715768c031d40f9b614
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-10 09:39:57 +0430

    update

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# HazelCast
+Hazelcast is an operational, in-memory, distributed computing platform for managing data and performing parallel execution for application speed and scale.
+
 # Discovery Server
 Eureka Server is an application that holds the information about all client-service applications. Every Micro service will register into the Eureka server and Eureka server knows all the client applications running on each port and IP address. Eureka Server is also known as Discovery Server.
 
---
+++ src/main/java/af/asr/discovery/Application.java
@@ -0,0 +1,19 @@
+package af.asr.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableConfigServer
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
