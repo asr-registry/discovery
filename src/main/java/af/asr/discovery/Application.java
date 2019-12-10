45958f8eb2733fb1b777fc808c8036c8a5216189
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-10 09:42:54 +0430

    hazelcast in-memory data grid

--- src/main/java/af/asr/discovery/Application.java
+++ src/main/java/af/asr/discovery/Application.java
@@ -6,10 +6,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@SpringBootApplication
-@EnableEurekaServer
-@EnableConfigServer
-@EnableAspectJAutoProxy(proxyTargetClass = true)
+@SpringBootApplication(scanBasePackages = {"af.asr.*"})
 public class Application {
 
 	public static void main(String[] args) {
