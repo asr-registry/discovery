af97618faeb8cb05a80fa6afc3a676964e503be9
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-10 09:41:55 +0430

    hazelcast in-memory data grid

---
+++ src/main/java/af/asr/discovery/config/EurekaServerConfig.java
@@ -0,0 +1,9 @@
+package af.asr.discovery.config;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEurekaServer
+public class EurekaServerConfig {
+}
